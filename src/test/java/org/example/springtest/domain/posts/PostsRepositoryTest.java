671a26d824dba5785173d0f8004bef47083eaaad
fomerna <fomerna@sbdc.or.kr>
2019-12-30 23:47:00 +0900

    롬복 어노테이션 DI 추가 Posts Test 추가

---
+++ src/test/java/org/example/springtest/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package org.example.springtest.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void postSaveAndGet() {
+        String title = "테스트 게시글";
+        String content = " TestContent";
+
+        postsRepository.save(
+                Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("fomerna")
+                        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+
+    }
+}
